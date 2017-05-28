b5cfab5a615dbbdef3c6ed47c35977d5c92b8c9d
Shaurabh <Shaurabh@Jarvis>
2017-05-28 19:10:52 +0545

    model

--- ecommerce/src/main/java/com/info/model/Category.java
+++ ecommerce/src/main/java/com/info/model/Category.java
@@ -1,5 +1,23 @@
 package com.info.model;
 
 public class Category {
+	
+	private int categoryid;
+	public int getCategoryid() {
+		return categoryid;
+	}
+	public void setCategoryid(int categoryid) {
+		this.categoryid = categoryid;
+	}
+	public String getCategoryname() {
+		return categoryname;
+	}
+	public void setCategoryname(String categoryname) {
+		this.categoryname = categoryname;
+	}
+	private  String categoryname;
+	
+	
+	
 
 }
