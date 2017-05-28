71a100751ed56122a9c510de5b68f020ee5b1ab7
Shaurabh <Shaurabh@Jarvis>
2017-05-28 17:21:36 +0545

    addition of some jsp pages

--- ecommerce/src/main/java/com/info/controller/CategoryController.java
+++ ecommerce/src/main/java/com/info/controller/CategoryController.java
@@ -1,5 +1,21 @@
 package com.info.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.info.service.CategoryService;
+
+@Controller
 public class CategoryController {
+	
+	
+	@Autowired
+	CategoryService categoryService;
+	
+	
+	
+	
+	
+	
 
 }
